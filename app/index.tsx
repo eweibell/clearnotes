71aa0c7d3231948c3d887cef7d9efa12536a0c53
ikanop <113450482+ikanop@users.noreply.github.com>
2026-02-06 13:47:50 +0100

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,39 @@
+import { Text, View, Image, Pressable } from "react-native";
+import { router } from "expo-router";
+
+export default function Index() {
+  return (
+    <View style={{ flex: 1 }}>
+        <Image source={require("../assets/images/clearnotes-logo2.png")}
+               style={{
+                   width: "100%",
+                   height: 300,
+                   resizeMode: "cover"
+        }}
+        />
+
+      <View
+          style={{
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center"
+      }}
+      >
+          <Pressable
+              onPress={() => router.push("/login")}
+              style={{
+                  width: 300,
+                  height: 100,
+                  borderColor: "#000",
+                  borderRadius: 20,
+                  backgroundColor: "#0081ff",
+                  justifyContent: "center",
+                  alignItems: "center"
+          }}
+          >
+            <Text style={{ fontSize: 45, color: "white", fontFamily: "SigmarOne-Regular" }}>Get Started!</Text>
+          </Pressable>
+        </View>
+    </View>
+  );
+}
