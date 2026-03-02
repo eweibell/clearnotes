3bedf6080bb2fd6bec05cd932f7875760c3b6eac
eweibell <54504318+eweibell@users.noreply.github.com>
2026-03-02 14:39:06 +0100

    Revert "Login system"

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,4 @@
 import { Stack } from "expo-router";
-import * as WebBrowser from "expo-web-browser";
-import { LogBox } from "react-native";
-
-import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
-import '@/global.css';
-
-WebBrowser.maybeCompleteAuthSession();
-if (__DEV__) {
-  LogBox.ignoreLogs([
-    "SafeAreaView has been deprecated and will be removed in a future release",
-  ]);
-}
 
 export default function RootLayout() {
   return (
