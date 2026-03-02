a64bd189460d1c369cf5ec9cfd5da07ab1a732de
eweibell <even@weibell.no>
2026-03-02 21:40:20 +0100

    Login system (#4 squashed)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,16 @@
 import { Stack } from "expo-router";
+import * as WebBrowser from "expo-web-browser";
+import { LogBox } from "react-native";
+
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
+import '@/global.css';
+
+WebBrowser.maybeCompleteAuthSession();
+if (__DEV__) {
+  LogBox.ignoreLogs([
+    "SafeAreaView has been deprecated and will be removed in a future release",
+  ]);
+}
 
 export default function RootLayout() {
   return (
