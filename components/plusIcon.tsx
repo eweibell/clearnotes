daac0409cac0a7ca7db2b0f9910e35d2db5093cd
ikanop <113450482+ikanop@users.noreply.github.com>
2026-03-19 13:50:53 +0100

    feat: rename MyComponent to PlusIcon in plusIcon.tsx

--- components/plusIcon.tsx
+++ components/plusIcon.tsx
@@ -1,6 +1,6 @@
 import { IconButton } from 'react-native-paper';
 
-const MyComponent = () => (
+const PlusIcon = () => (
   <IconButton
     icon="plus"
     iconColor={"black"}
@@ -9,4 +9,4 @@ const MyComponent = () => (
   />
 );
 
-export default MyComponent
+export default PlusIcon
